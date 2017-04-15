f759ec940da2db96184c5bc5572f1486177f5e93
eab6863 <eab6863@gmail.com>
2017-04-14 21:53:52 -0400

    Sandbox for magnet and metal object

---
+++ Assets/MagnetConstantForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Polarity
+{
+    Positive = 1,
+    Negative = -1
+}
+public class MagnetConstantForce : MonoBehaviour
+{
+    public float force;
+    public Polarity polarity;
+    Vector3 direction;
+
+    private void Update()
+    {
+        direction = transform.up;
+    }
+
+    public Vector3 GetForce()
+    {
+        return direction * (force * (int)polarity);
+    }
+}
---
+++ Assets/MetalObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class MetalObject : MonoBehaviour
+{
+    Rigidbody rb;
+    public Polarity polarity;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag.Equals("Magnet"))
+        {
+            rb.AddForce(other.GetComponent<MagnetConstantForce>().GetForce() * (int)polarity, ForceMode.Force);
+        }
+    }
+}
